eaf25bf3888d1a0bf5f6ce208c7451dbe1615d5c
sonyrahul520 <39365623+sonyrahul520@users.noreply.github.com>
2022-01-23 18:23:16 +0000

    Adding model classes

---
+++ Models/WebinarRegisters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webinar_Register.Models
+{
+    public class WebinarRegisters
+    {
+        
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+        public string Email { get; set; }
+
+        public bool? WillJoin { get; set; }
+    }
+}
